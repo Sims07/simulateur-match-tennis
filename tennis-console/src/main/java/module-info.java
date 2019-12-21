4504cc8b970cdefab6df84a19347775bd1be8b53
Simon Chareyron <simonchareyron@MacBook-Pro-de-Simon.local>
2019-12-20 20:38:00 -0500

    Display score change with delay using reactor library

--- tennis-console/src/main/java/module-info.java
+++ tennis-console/src/main/java/module-info.java
@@ -4,4 +4,6 @@
 module tennis.console {
     exports simon.chareyron.tennis.console;
     requires tennis.usecase;
+    requires reactor.core;
+    requires org.reactivestreams;
 }
--- tennis-console/src/main/java/simon/chareyron/tennis/console/TennisMainConsole.java
+++ tennis-console/src/main/java/simon/chareyron/tennis/console/TennisMainConsole.java
@@ -1,8 +1,13 @@
 package simon.chareyron.tennis.console;
 
+import reactor.core.publisher.Flux;
 import simon.chareyron.tennis.usecase.SimulateTennisMatchOutput;
 import simon.chareyron.tennis.usecase.SimulateTennisMatchUseCaseImpl;
 import simon.chareyron.tennis.usecase.model.TennisScoreModel;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 /**
  * @author djz4712
@@ -10,16 +15,20 @@ import simon.chareyron.tennis.usecase.model.TennisScoreModel;
 public class TennisMainConsole {
 
     public static void main(String[] args) {
-        new SimulateTennisMatchUseCaseImpl(new SimulateTennisMatchOutput() {
-            @Override
-            public void onScoreChanged(int i, TennisScoreModel tennisScoreModel) {
-                System.out.println("Player:" + i + " win the point");
-            }
+        Flux<Object> objectFlux = Flux.create(fluxSink -> {
+            new SimulateTennisMatchUseCaseImpl(new SimulateTennisMatchOutput() {
+                @Override
+                public void onScoreChanged(int i, TennisScoreModel tennisScoreModel) {
+                    fluxSink.next("Player:" + i + " win the point");
+                }
 
-            @Override
-            public void onPlayerWinTheMatch(int i, TennisScoreModel tennisScoreModel) {
-                System.out.println("Player:" + i + " win the match");
-            }
-        }).playRandomMatch(2);
+                @Override
+                public void onPlayerWinTheMatch(int i, TennisScoreModel tennisScoreModel) {
+                    fluxSink.next("Player:" + i + " win the match");
+                }
+            }).playRandomMatch(2);
+        }).delayElements(Duration.of(500, ChronoUnit.MILLIS));
+        objectFlux.subscribe(System.out::println);
+        objectFlux.blockLast();
     }
 }
